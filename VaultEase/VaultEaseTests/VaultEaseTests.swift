7b5897396d955b00861d6d37834755595adae1e1
Christian Castro <fernandochris1995@gmail.com>
2025-10-19 11:16:03 -0500

    ✨ Add new project in IOS

---
+++ VaultEase/VaultEaseTests/VaultEaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  VaultEaseTests.swift
+//  VaultEaseTests
+//
+//  Created by ChristianCastro on 10/19/25.
+//
+
+import Testing
+@testable import VaultEase
+
+struct VaultEaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
